7d232d5c15de8225996ba114e573158856977c97
Devil9766 <ashasharma10883@gmail.com>
2025-08-28 13:36:44 +0530

    small update

--- ChartAI/src/Components/api.js
+++ ChartAI/src/Components/api.js
@@ -1,10 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL : "http://localhost:3000",
+    baseURL : "https://chartai-1sd0.onrender.com",
     withCredentials : true
 })
 
 export default api;
 
-// https://chartai-1sd0.onrender.com
